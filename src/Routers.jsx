1e17f43ae8024029a9178812c8031143e90c93dd
Yolanda <ylopez.tech@gmail.com>
2025-03-15 14:19:16 +0100

    Introducción de rutas

---
+++ src/Routers.jsx
@@ -0,0 +1,28 @@
+// import { Navigate, createBrowserRouter } from 'react-router-dom'
+// // import {About, Home, Login, MainApp } from './09-use-Context/index'
+// import { Children } from 'react'
+
+// export const getRoutes = () => createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <MainApp />,
+//         children: [
+//             {
+//                 path: "/",
+//                 element: <Home />
+//             },
+//             {
+//                 path: "login",
+//                 element: <Login />
+//             },
+//             {
+//                 path: "about",
+//                 element: <About />
+//             },
+//             {
+//                 path: "",
+//                 element: <Navigate to="/" replace />
+//             }
+//         ]
+//     }
+// ])
